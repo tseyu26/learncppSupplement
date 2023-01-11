ba64c18e6b6af198b7b7c0ebee4fef81824f4c00
tseyu26 <newbeeu2022@outlook.com>
2023-01-11 23:17:17 +0800

    add typealisname

---
+++ typealisname/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by tseyu on 1/11/23.
+//
+#include <iostream>
+
+int main(){
+    using Distance = double;
+
+    Distance milesToDestination {3.4};
+
+    std::cout << milesToDestination << "\n";
+
+    return 0;
+}
