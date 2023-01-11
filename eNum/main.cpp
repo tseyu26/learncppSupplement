7498f11b9e9ff132d2418e1da46f1ff80f4dba5a
tseyu26 <newbeeu2022@outlook.com>
2023-01-11 23:46:26 +0800

    add eNum

---
+++ eNum/main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by tseyu on 1/11/23.
+//
+enum Color{
+    red,
+    green,
+    blue,
+};
+
+int main(){
+
+    Color apple { red };
+    Color shirt { green };
+    Color cup { blue };
+
+    // Color socks { white };
+    // Color hat { 2 };
+
+    return 0;
+}
