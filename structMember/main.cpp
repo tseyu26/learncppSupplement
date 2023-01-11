68cb916beb810cae21788b3108dea3d445331d92
tseyu26 <newbeeu2022@outlook.com>
2023-01-12 00:46:59 +0800

    add structMember

---
+++ structMember/main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by tseyu on 1/12/23.
+//
+#include <iostream>
+
+struct Employee{
+    int id {};
+    int age {};
+    double wage {};
+};
+
+int main(){
+    Employee joe;
+
+    joe.age = 32;
+
+    std::cout << joe.age << "\n";
+
+    return 0;
+}
