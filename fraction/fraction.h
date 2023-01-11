56b57503eb35bc67647efcb46a1dc097de10bf76
tseyu26 <newbeeu2022@outlook.com>
2023-01-11 23:38:02 +0800

    add fraction

---
+++ fraction/fraction.h
@@ -0,0 +1,11 @@
+//
+// Created by tseyu on 1/11/23.
+//
+
+#ifndef FRACTION_FRACTION_H
+#define FRACTION_FRACTION_H
+struct Fraction{
+    int numerator{};
+    int denominator{};
+};
+#endif //FRACTION_FRACTION_H
---
+++ fraction/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by tseyu on 1/11/23.
+//
+#include "fraction.h"
+
+int main(){
+    Fraction f{ 3,4 };
+
+    return 0;
+}
